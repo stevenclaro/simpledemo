30b902eb3130f225e20c81d75f5bc8a58d0ccb17
StevenJunior12 <stevenjunior2025@126.com>
2026-04-08 09:10:39 +0800

    Update hello.py
    provide add function

--- hello.py
+++ hello.py
@@ -2,5 +2,11 @@
 def say_hello():
     print("Hello, Open Source!")
 
+# 新增：加法函数
+def add(a, b):
+    return a + b
+    
 if __name__ == "__main__":
     say_hello()
+    # 调用加法函数演示
+    print("10 + 20 =", add(10, 20))
